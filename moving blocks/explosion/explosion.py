30c08efe07d965febda29389b524a5f4c410f34f
Tom Lynch <twhlynch@gmail.com>
2022-10-14 13:01:29 +1100

    json annoying

--- moving blocks/explosion/explosion.py
+++ moving blocks/explosion/explosion.py
@@ -52,20 +52,25 @@ def animate_object(node, px, py, pz, xmax, zmax):
     }
     return group_json
 
-with open('level.json') as json_file:
+with open('smrgsbd.json') as json_file:
     json = json.load(json_file)
     xmax = 0
     zmax = 0
     for node in json["levelNodes"]:
-        if math.sqrt(node["position"]["x"] ** 2) > xmax:
-            xmax = math.sqrt(node["position"]["x"] ** 2)
-        if math.sqrt(node["position"]["y"] ** 2) > zmax:
-            zmax = math.sqrt(node["position"]["y"] ** 2)
+        try:
+            if math.sqrt(node["levelNodeStatic"]["position"]["x"] ** 2) > xmax:
+                xmax = math.sqrt(node["levelNodeStatic"]["position"]["x"] ** 2)
+            if math.sqrt(node["levelNodeStatic"]["position"]["y"] ** 2) > zmax:
+                zmax = math.sqrt(node["levelNodeStatic"]["position"]["y"] ** 2)
+        except:
+            pass
     for node in json["levelNodes"]:
-        x = node["position"]["x"]
-        y = node["position"]["y"]
-        z = node["position"]["z"]
-        node = animate_object(node, x, y, z, xmax, zmax)
-
-with open('level.json', 'w') as outfile:
+        try:
+            x = node["levelNodeStatic"]["position"]["x"]
+            y = node["levelNodeStatic"]["position"]["y"]
+            z = node["levelNodeStatic"]["position"]["z"]
+            node = animate_object(node, x, y, z, xmax, zmax)
+        except:
+            pass
+with open('explosion.json', 'w') as outfile:
     json.dump(json, outfile, indent=4)
